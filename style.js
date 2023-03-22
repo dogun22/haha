411b6f2ab7c84c9305493d7ce60ad32e72c5c06a
도건 <gmldyd013@naver.com>
2023-03-22 23:38:44 +0900

    3rd commit

---
+++ style.js
@@ -0,0 +1,4 @@
+$(function () {
+  // $('#main_New span, #main_Man span, #main_leather span').addClass('start')
+  $('#main_woman span, #main_new span, #main_man span, #main_leather span, #main_adidas span, #main_summer span').removeClass('start');
+});
